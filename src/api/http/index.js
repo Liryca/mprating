c842df664dbb15bc8e3360528e4d75f102c83226
Tarasova Anna <tarasavahanna@gmail.com>
2023-07-25 19:15:37 +0300

    commit

--- src/api/http/index.js
+++ src/api/http/index.js
@@ -5,7 +5,7 @@ import axios from "axios";
 export const $instance = axios.create({
 
     // withCredentials: true,
-    baseURL: 'http://auth.mprating.ru:8765/index.html',
+    // baseURL: 'http://auth.mprating.ru:8765/',
     // http://ovz21.j90211046.m6zkp.vps.myjino.ru:49156/get_client?client_id=2
     headers: {
         'Accept': 'application/json',
